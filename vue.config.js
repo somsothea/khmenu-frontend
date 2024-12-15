d757c08303e5bd93435eb8aa95407a125d790b9b
Som Sothea <somsothea@gmail.com>
2024-12-16 00:07:50 +0700

    Frontend Vue JS
    Khmer Digital Menu Vue JS connect with API Express JS

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    //proxy: 'http://localhost:4000'
+    //proxy: 'http://192.168.1.21:4000/'
+    proxy: process.env.VUE_APP_URL
+  }
+})
